54772cf5f392fa7c9deaa380fcd8b2760425592f
Igor Kroshkin <igor.krosh@gmail.com>
2020-09-24 16:43:04 +0300

    Navbar.vue - верстка

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Navbar/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,9 +9,23 @@
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: 'Jost', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Navbar.vue
@@ -0,0 +1,97 @@
+<template>
+    <b-navbar wrapper-class="container">
+        <template slot="brand">
+            <a class="logo">
+                <img src="../assets/images/logo.svg" alt="Star Wars Logo">
+            </a>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                <span>История</span>
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="contacts-icons" href="https://tlgg.ru/igorkrosh">
+                        <img src="../assets/images/icons/telegram.svg" alt="">
+                    </a>
+                    <a class="contacts-icons" href="https://github.com/igorkrosh">
+                        <img src="../assets/images/icons/github.svg" alt="">
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+}
+</script>
+
+<style>
+#app .navbar
+{
+    background-color: #242424;
+}
+#app .navbar-brand img
+{
+    height: 50px;
+    max-height: initial;
+    margin: auto;
+}
+
+.logo
+{
+    display: flex;
+    margin-right: 15px;
+}
+
+#app .navbar-item
+{
+    font-size: 18px;
+    color: #ffffff;
+}
+
+#app .navbar-item:hover
+{
+    background-color: initial;
+}
+
+#app .navbar-item span
+{
+    position: relative;
+}
+
+#app .navbar-item span:after
+{
+    content: "";
+    position: absolute;
+    height: 2px;
+    width: 0px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #ffffff;
+    transition: 0.5s;
+}
+
+#app .navbar-item:hover span:after
+{
+    width:100%;
+}
+
+#app .contacts-icons img
+{
+    height: 36px;
+    max-height: initial;
+}
+
+.contacts-icons
+{
+    margin: 0 5px;
+}
+</style>
